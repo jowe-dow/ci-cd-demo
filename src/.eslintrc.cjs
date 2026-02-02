d92976f92e49c3f8f99d249f34fd3c52b090cc4b
Liu <yinbing.liu@xiaobao100.compnpm>
2026-02-02 15:11:43 +0800

    feat: add

---
+++ src/.eslintrc.cjs
@@ -0,0 +1,21 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
